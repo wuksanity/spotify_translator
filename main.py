52bab399f71ce88da7f7536ec41ac2df53b1bfdc
Walker Riley <riley.855@osu.edu>
2022-11-22 23:02:19 -0500

    developer link established

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# first commit
+from googletrans import Translator
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+ID = "b5b1c80530944fe1b480d22d176a80ee"
+secret = "b8501a850d3048fcb83d4ca86d64a1a7"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=ID,
+                                               client_secret=secret,
+                                               redirect_uri="http://localhost:8888/callback",
+                                               scope="user-read-currently-playing"))
+
+print(sp.currently_playing())
